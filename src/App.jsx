ee4f6ac51e29e156bdc33fe4ddb4464c649e03de
Jose Valdez <jvaldez-fernandez@csumb.edu>
2026-06-03 10:39:04 -0700

    Moved products array to its own file and out of App.jsx to make it cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,9 @@ import './App.css'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import ProductCard from './components/ProductCard.jsx'
+import products from './data/products.js'
 
 function App() {
-
-const products = [
-  { id: 1, 
-    name: "Dress",
-    price: 25,
-    isAvailable: true
-  }, 
-  { id: 2,
-    name: "Jeans",
-    price: 30,
-    isAvailable: true
-  },
-  {
-    id: 3,
-    name: "Shoes",
-    price: 50,
-    isAvailable: false
-  }
-];
 
   return (
     <div>
---
+++ src/data/products.js
@@ -0,0 +1,20 @@
+const products = [
+  { id: 1, 
+    name: "Dress",
+    price: 25,
+    isAvailable: true
+  }, 
+  { id: 2,
+    name: "Jeans",
+    price: 30,
+    isAvailable: true
+  },
+  {
+    id: 3,
+    name: "Shoes",
+    price: 50,
+    isAvailable: false
+  }
+];
+
+export default products
