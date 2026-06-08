86b27a9e9f59426eb2599567a19f81155d32b1f1
Jose Valdez <jvaldez-fernandez@csumb.edu>
2026-06-07 19:33:03 -0700

    Added filter by category buttons

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,44 @@
 import products from '../data/products.js'
 import ProductCard from '../components/ProductCard.jsx'
-
+import {useState} from 'react'
+import '../styles/Products.css'
 
 function Product () {
+    const [selectedCategory, setSelectedCategory] = useState("all");
+
+    const updateSelectedCategory = (e) => {
+        setSelectedCategory(e.target.value);
+    }
+
+    const filteredProducts = selectedCategory == "all" ? products : 
+        products.filter((product) => product.category === selectedCategory);
+
     return (
     <>
+    <label className="radioGroup">
+        <label>
+            <input type="radio" name="category" value="all" onChange={updateSelectedCategory}/> All
+        </label>
+        <label>
+            <input type="radio" name="category" value="Dresses" onChange={updateSelectedCategory}/> Dresses
+        </label>
+        <label>
+            <input type="radio" name="category" value="Jeans" onChange={updateSelectedCategory}/> Jeans
+        </label>
+        <label>
+            <input type="radio" name="category" value="Self-care" onChange={updateSelectedCategory}/> Self-Care
+        </label>
+        <label>
+            <input type="radio" name="category" value="Jackets" onChange={updateSelectedCategory}/> Jackets
+        </label>
+        <label>
+            <input type="radio" name="category" value="Shirts" onChange={updateSelectedCategory}/> Shirts
+        </label>
+    </label>
+
         <h1 id="productosHeading">Productos De Venta: </h1>
         <div className="productDetails">
-          {products.map((product) => (
+          {filteredProducts.map((product) => (
             <ProductCard key={product.id} product={product}/>
           ))}
         </div>
--- src/data/products.js
+++ src/data/products.js
@@ -3,12 +3,14 @@ const products = [
     name: "Dress",
     price: 25,
     image: "https://media.istockphoto.com/id/1440977634/photo/vertical-shot-of-the-beautiful-pink-dress-isolated-on-the-white-background.jpg?s=612x612&w=0&k=20&c=3lSPxjs-xh7PGeW2eGnPlawyw9gcJPlnc1QWnwnkNyI=",
+    category: "Dresses",
     isAvailable: true
   }, 
   { id: 2,
     name: "Jeans",
     price: 30,
     image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1736527960-untitled-3-67815035563fa.jpg?crop=0.774xw:0.830xh;0.111xw,0.0871xh&resize=980:*",
+    category: "Jeans",
     isAvailable: true
   },
   {
@@ -16,6 +18,7 @@ const products = [
     name: "Shoes",
     price: 50,
     image: "https://www.verywellfit.com/thmb/9VMfH5UMm2JBWvK-Gz7FZRVzr0U=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vwt-asics-womens-gel-pulse-16-jinfeng-chen-21-712f0f140e834b84b3a8dcf0bf154771.jpeg",
+    category: "Shoes",
     isAvailable: false
   }
 ];
